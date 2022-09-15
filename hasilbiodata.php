b33d18ffee098ce71e960e4c56ee97ceeabb7a5d
Rsyy <101184919+rsyfariz@users.noreply.github.com>
2022-09-15 07:18:04 +0700

    Add files via upload

---
+++ hasilbiodata.php
@@ -0,0 +1,17 @@
+
+<html>
+<body>
+
+Nama :<?php echo $_POST["nama"]; ?><br>
+Gender :<?php echo $_POST["gender"]; ?><br>
+Agama :<?php echo $_POST["agama"]; ?><br>
+Alamat :<?php echo $_POST["alamat"]; ?><br>
+Tempat Lahir :<?php echo $_POST["tempat"]; ?><br>
+Tanggal Lahir :<?php echo $_POST["tanggal"]; ?><br>
+Hobi :<?php echo $_POST["hobi"]; ?><br>
+Cita cita :<?php echo $_POST["cita"]; ?><br>
+Usia :<?php echo $_POST["usia"]; ?><br>
+Asal sekolah :<?php echo $_POST["asl"]; ?><br>
+
+</body>
+</html>
